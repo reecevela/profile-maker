6b121cd22be463cfa524db5c3d68d5a66d95d915
Reece Vela <101456455+reecevela@users.noreply.github.com>
2022-12-04 19:59:55 -0600

    Update display_profile.js

--- display_profile.js
+++ display_profile.js
@@ -2,10 +2,10 @@
 
 $(document).ready( () => {
 	// display data from session storage
-	$('.email').text(sessionStorage.getItem('email'));
-	$('.phone').text(sessionStorage.getItem('phone'));
-	$('.zip').text(sessionStorage.getItem('zip'));
-	$('.dob').text(sessionStorage.getItem('dob'));
+	$('#email').text(sessionStorage.getItem('email'));
+	$('#phone').text(sessionStorage.getItem('phone'));
+	$('#zip').text(sessionStorage.getItem('zip'));
+	$('#dob').text(sessionStorage.getItem('dob'));
 
 	$('#back').click( () => {
 			history.back();
